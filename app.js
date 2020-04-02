cb8d03c37b289b66ecfc65a4bef13e55b16eb018
Kai Wegner <kai.wegner@exozet.com>
2020-04-02 16:38:19 +0200

    api now uses authentication

--- app.js
+++ app.js
@@ -2,14 +2,54 @@ const Express = require("express");
 const Cors = require("cors");
 const Mongoose = require("mongoose");
 const BodyParser = require("body-parser");
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
 
-var app = Express();
 
-app.use(Cors());
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
 
-Mongoose.connect("mongodb://localhost/haushalt");
+
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+    function(username, password, done) {
+      UserModel.findOne({"username" : username}, function(err, user){
+        if(err) {return done(err);}
+        if(!user) {return done(null, false,     {message:"incorrect username. "})}
+        if(!validPassword(user,password)) {
+            return done(null, false, {message:"incorrect password. "});
+        }
+        return done(null, user);
+      });
+    }));
+  
+  
+  // Configure Passport authenticated session persistence.
+  //
+  // In order to restore authentication state across HTTP requests, Passport needs
+  // to serialize users into and deserialize users out of the session.  The
+  // typical implementation of this is as simple as supplying the user ID when
+  // serializing, and querying the user record by ID from the database when
+  // deserializing.
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    //If using Mongoose with MongoDB; if other you will need JS specific to that schema.
+    UserModel.findById(id, (err, user) => {
+        done(err, user);
+    });
+  });
+
+  const UserModel = Mongoose.model("user",{
+      username:String,
+      password:String
+  });
 
 const BillModel = Mongoose.model("bill",{
     date:Date,
@@ -18,9 +58,36 @@ const BillModel = Mongoose.model("bill",{
     category:String
 });
 
+function validPassword(user, password)
+{
+    return user.password == password;
+}
+var app = Express();
 
+app.use(Cors());
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'haushaltssperre', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post("/bill", async (request, response) => {
+Mongoose.connect("mongodb://localhost/haushalt");
+
+  
+app.post("/login", passport.authenticate('local', { failureRedirect: '/login' }), (request, response) => {
+    try{
+        response.send({"message":"ok"});
+    }catch(error){
+        response. status(500).send(error);
+    }
+});
+
+app.post("/logout", async (req, res) => {
+    req.logout();
+    res.send({"message":"ok"});
+});
+
+app.post("/bill",require('connect-ensure-login').ensureLoggedIn(), async (request, response) => {
     try {
         var bill = new BillModel(request.body);
         var result = await bill.save();
@@ -30,7 +97,7 @@ app.post("/bill", async (request, response) => {
     }
 });
 
-app.get("/bill/:id", async (request, response) => {
+app.get("/bill/:id",require('connect-ensure-login').ensureLoggedIn(), async (request, response) => {
     try {
         var bill = await BillModel.findById(request.params.id).exec();
         response.send(bill);
@@ -39,7 +106,7 @@ app.get("/bill/:id", async (request, response) => {
     }
 });
 
-app.put("/bill/:id", async (request, response) => {
+app.put("/bill/:id",require('connect-ensure-login').ensureLoggedIn(), async (request, response) => {
     try {
         var bill = await BillModel.findById(request.params.id).exec();
         bill.set(request.body);
@@ -50,7 +117,7 @@ app.put("/bill/:id", async (request, response) => {
     }
 });
 
-app.delete("/bill/:id", async (request, response) => {
+app.delete("/bill/:id",require('connect-ensure-login').ensureLoggedIn(), async (request, response) => {
     try {
         var result = await BillModel.deleteOne({ _id: request.params.id }).exec();
         response.send(result);
@@ -60,7 +127,7 @@ app.delete("/bill/:id", async (request, response) => {
 });
 
 
-app.get("/bills", async (request, response) => {
+app.get("/bills",require('connect-ensure-login').ensureLoggedIn(), async (request, response) => {
     try {
         var result = await BillModel.find().lean().exec();
         response.send(result);
