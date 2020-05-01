bcca30bcc2a3a0c4f7da65f188dc674a63ea1001
Erik Wegner <E_Wegner@web.de>
2020-05-02 00:08:41 +0200

    Refactoring

--- server/api/controllers/auth/controller.ts
+++ server/api/controllers/auth/controller.ts
@@ -1,19 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import * as HttpStatus from 'http-status-codes';
 import UserService from '../../services/user.service';
-import { hashPassword, User, toProfile, IUserModel } from '../../../api/models/user';
+import { hashPassword, toProfile, IUserModel } from '../../../api/models/user';
 import { sign } from 'jsonwebtoken';
 import passport from 'passport';
 
 export class Controller {
-    async oauthCallbackGoogle(req: Request, res: Response, next: (err: any) => void) {
+    private static generateTokenResponse(res: Response, user: IUserModel) {
+        const userProfile = toProfile(user);
+        const payload = { id: user._id };
+        const token = sign(payload, process.env.JWT_KEY);
+        res.json({ message: "ok", token: token, userProfile: userProfile });
+    }
+
+    async oauthCallbackGoogle(req: Request, res: Response, next: NextFunction) {
         passport.authenticate('google', function (err, user, info, status) {
             if (err) { return next(err) }
             if (!user) { return res.redirect('/signin') }
-            const userProfile = toProfile(user);
-            const payload = { id: user._id };
-            const token = sign(payload, process.env.JWT_KEY);
-            res.json({ message: "ok", token: token, userProfile: userProfile });
+            Controller.generateTokenResponse(res, user);
         })(req, res, next);
     }
 
@@ -22,11 +26,7 @@ export class Controller {
             const user = await UserService.findByName(request.body.username);
             const hash = hashPassword(request.body.password);
             if (user?.password == hash) {
-                // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-                const userProfile = toProfile(user);
-                const payload = { id: user._id };
-                const token = sign(payload, process.env.JWT_KEY);
-                response.json({ message: "ok", token: token, userProfile: userProfile });
+                Controller.generateTokenResponse(response, user);
             } else {
                 response.status(HttpStatus.UNAUTHORIZED).json({ "message": "invalid password" });
             }
