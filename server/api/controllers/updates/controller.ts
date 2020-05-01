0bd89f98d337b5dab60cb594cb1f4b6bc0d25667
Kai Wegner <kwnetzwelt@gmail.com>
2020-05-01 21:51:39 +0200

    updates call is now in a separate router

---
+++ server/api/controllers/updates/controller.ts
@@ -0,0 +1,24 @@
+import EntryService from '../../services/entry.service';
+import { Request, Response, NextFunction } from 'express';
+import * as HttpStatus from 'http-status-codes';
+import { UserProfile } from '../../models/user';
+import { EntryArrayResponse } from '../../models/entry';
+
+export class Controller {
+
+  async all(req: Request, res: Response, next: NextFunction) {
+    try {
+      const delta = Math.min(Math.max(0,parseInt(String(req.query.updatedMillisecondsAgo))),2*60*1000);
+      const updatedAt = new Date(new Date().getTime() - delta);
+      const result = await EntryService.updated((req.user as UserProfile).groupId,updatedAt);
+      const response: EntryArrayResponse = {data: result};
+      return res.status(HttpStatus.OK).json(response);
+    }
+    catch (err) {
+      return next(err);
+    }
+  }
+
+}
+
+export default new Controller();
--- server/routes.ts
+++ server/routes.ts
@@ -2,10 +2,12 @@ import { Application } from 'express';
 import entryRouter from './api/controllers/entry/router';
 import targetRouter from './api/controllers/targets/router';
 import invitesRouter from './api/controllers/invites/router';
+import updatesRouter from './api/controllers/updates/router';
 import {loginRouter, logoutRouter} from './api/controllers/auth/router';
 
 export default function routes(app: Application): void {
   app.use('/api/v1/bills', entryRouter);
+  app.use('/api/v1/updates', updatesRouter);
   app.use('/api/v1/targets', targetRouter);
   app.use('/api/v1/invites', invitesRouter);
   app.use('/api/v1/login', loginRouter);
